4039a5b88556eae93d4c4ed976f44d350b1c9a58
bommareddythanmayasree <thanmayasree09@gmail.com>
2025-04-05 15:51:19 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Construct the correct path to the model file
 model_path = os.path.join(current_directory, "drug_discovery_model.joblib")
-git commit -m "Added trained drug discovery model"
+
 
 # Load the model
 model = joblib.load(model_path)
